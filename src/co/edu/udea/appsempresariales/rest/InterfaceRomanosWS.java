27e1540ad60e4e75376b8bbe3fccb9bbc6d5e2b9
farleyrua <farley.rua@gmail.com>
2015-02-06 21:33:30 -0500

    Agregando interface del servicio

---
+++ src/co/edu/udea/appsempresariales/rest/InterfaceRomanosWS.java
@@ -0,0 +1,9 @@
+package co.edu.udea.appsempresariales.rest;
+
+import co.edu.udea.appsempresariales.dto.NumeroRomano;
+
+public interface InterfaceRomanosWS {
+	
+	public NumeroRomano convertirARomano(int numero);
+
+}
