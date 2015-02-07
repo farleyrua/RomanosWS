c203df668f6272687d291a8ddac47e0ace77670d
farleyrua <farley.rua@gmail.com>
2015-02-06 21:36:26 -0500

    Agregando Path del servicio web

---
+++ src/co/edu/udea/appsempresariales/rest/PathWS.java
@@ -0,0 +1,14 @@
+package co.edu.udea.appsempresariales.rest;
+
+public class PathWS {
+	
+	private PathWS() {
+		super();
+	}
+
+		public class ConvertidorRomanoWSContext {
+	
+			public static final String ROOT_PATH = "/convertirRomanos";
+			public static final String PARAMETRO = "numero";
+		}
+}
